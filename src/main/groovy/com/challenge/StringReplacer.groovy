6fd1f148e561f2e54c55fb92b25944cf73d5367b
Jan Benda <bendajan@centrum.cz>
2024-04-05 17:14:43 +0200

    replacing text in all files in directory and its directories

--- src/main/groovy/com/challenge/StringReplacer.groovy
+++ src/main/groovy/com/challenge/StringReplacer.groovy
@@ -1,14 +1,19 @@
 package com.challenge
 
 import java.util.regex.Pattern
+import groovy.io.FileType
+
 
 class StringReplacer {
     static void main(String[] args) {
+        // reading user's inputs
+        // https://stackoverflow.com/questions/10184091/groovy-console-read-input
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
 
         print "Enter directory: "
         String dirInput = br.readLine()
+        def files = getFiles(dirInput)
 
         print "Enter searched text or pattern: "
         String textInput = br.readLine()
@@ -16,15 +21,35 @@ class StringReplacer {
         print "Enter replacement text: "
         String replacementInput = br.readLine()
 
-        replace(dirInput, textInput, replacementInput)
+        replace(files, textInput, replacementInput)
     }
 
-    static def replace(String path, String originalText, String newText) {
-        def fileContents = new File(path).text
-        Pattern pattern = ~originalText
-        fileContents = fileContents.replaceAll(pattern, newText)
 
-        println fileContents
+    static File[] getFiles(String path) {
+        // getting a list of files in given directory and subdirectories
+        // https://stackoverflow.com/questions/3953965/get-a-list-of-all-the-files-in-a-directory-recursive
+
+        //TODO: check if given string is a directory
+        def list = []
+
+        def dir = new File(path)
+        dir.eachFileRecurse(FileType.FILES) { file ->
+            println file
+            list << file
+        }
+        return list
+    }
+
+    static def replace(File[] files, String originalText, String newText) {
+        // iterating through each file and replacing given text/pattern
+        // https://www.tutorialspoint.com/groovy/groovy_file_io.htm
+
+        for (File f in files) {
+            Pattern pattern = ~originalText
+            def fileContent = f.text
+            fileContent = fileContent.replaceAll(pattern, newText)
+            f.withWriter { writer -> writer.writeLine(fileContent) }
+        }
 
     }
 }
