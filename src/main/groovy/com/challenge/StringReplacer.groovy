3b279a03361b8c478eedb74f9ec2d60b4b0043f8
Jan Benda <bendajan@centrum.cz>
2024-04-05 16:10:35 +0200

    added simple inputs, can be run in command line

--- src/main/groovy/com/challenge/StringReplacer.groovy
+++ src/main/groovy/com/challenge/StringReplacer.groovy
@@ -1,17 +1,30 @@
 package com.challenge
 
+import java.util.regex.Pattern
+
 class StringReplacer {
     static void main(String[] args) {
-        String filePath = "file/path"
-        String fileContents = new File(filePath).text
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+
+        print "Enter directory: "
+        String dirInput = br.readLine()
+
+        print "Enter searched text or pattern: "
+        String textInput = br.readLine()
+
+        print "Enter replacement text: "
+        String replacementInput = br.readLine()
+
+        replace(dirInput, textInput, replacementInput)
+    }
+
+    static def replace(String path, String originalText, String newText) {
+        def fileContents = new File(path).text
+        Pattern pattern = ~originalText
+        fileContents = fileContents.replaceAll(pattern, newText)
 
         println fileContents
 
-        def regex = "input string"
-        def pattern = ~regex
-
-        fileContents = fileContents.replaceFirst(pattern, "replacement")
-
-        println fileContents
     }
 }
