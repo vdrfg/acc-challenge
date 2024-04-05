cfd8ad11890d11661a90b3d673f81acbb9f770b0
Jan Benda <bendajan@centrum.cz>
2024-04-05 12:25:36 +0200

    basic concept for text replacing in given path

--- src/main/groovy/com/challenge/StringReplacer.groovy
+++ src/main/groovy/com/challenge/StringReplacer.groovy
@@ -2,6 +2,16 @@ package com.challenge
 
 class StringReplacer {
     static void main(String[] args) {
+        String filePath = "file/path"
+        String fileContents = new File(filePath).text
 
+        println fileContents
+
+        def regex = "input string"
+        def pattern = ~regex
+
+        fileContents = fileContents.replaceFirst(pattern, "replacement")
+
+        println fileContents
     }
 }
