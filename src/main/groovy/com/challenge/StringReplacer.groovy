329ea2abd99a20c8dba8fbb562bc081b04ab154b
Jan Benda <bendajan@centrum.cz>
2024-04-06 12:36:16 +0200

    logging modified files if optional argument passed

--- src/main/groovy/com/challenge/StringReplacer.groovy
+++ src/main/groovy/com/challenge/StringReplacer.groovy
@@ -1,27 +1,36 @@
 package com.challenge
 
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
 import java.util.regex.Pattern
 import groovy.io.FileType
 
 
 class StringReplacer {
     static void main(String[] args) {
-        // reading user's inputs
-        // https://stackoverflow.com/questions/10184091/groovy-console-read-input
 
+        // reading input arguments
         def fileDir = args[0]
         def origText = args[1]
         def newText = args[2]
-        def logDir = null
+
+        // log directory optional, hence null by default
+        def logPath = null
         if (args.length > 3) {
-            logDir = args[3]
+            logPath = args[3]
         }
 
         def files = getFiles(fileDir)
 
-        replace(files, origText, newText, logDir)
+        // checking if given directory is valid and/or if there are any files
+        if (files == null) {
+            print "Not a valid directory. Please try again."
+        } else if (files.length == 0) {
+            print "Directory is empty. No files to modify."
+        } else {
+            replace(files, origText, newText, logPath)
+        }
     }
-
 
     static File[] getFiles(String path) {
         // getting a list of files in given directory and subdirectories via recursive method
@@ -33,20 +42,45 @@ class StringReplacer {
         if (!dir.isDirectory()) {
             return null
         }
+
         dir.eachFileRecurse(FileType.FILES) { file ->
-            println file // TODO: remove once done
             list << file
         }
+
         return list
     }
 
-    static replace(File[] files, String origText, String newText, String logDir) {
+    static replace(File[] files, String origText, String newText, String logPath) {
+
+        def logFile = null
+
+        // if optional argument passed, log file is created
+        if (logPath != null) {
+
+            logFile = new File(logPath)
+
+            if (!logFile.exists()) {
+
+                // checking if log directory exists and creating one if it doesn't yet
+                def logDir = new File(logPath.substring(0, logPath.lastIndexOf("/")))
+                if (!logDir.isDirectory()) {
+                    logDir.mkdir()
+                }
+
+                logFile.createNewFile()
+            }
+        }
+
         // iterating through each file and replacing given text/pattern
         // https://www.tutorialspoint.com/groovy/groovy_file_io.htm
-
         for (File f in files) {
             Pattern pattern = ~origText
             def fileContent = f.text
+
+            if (fileContent.contains(origText) && logFile != null) {
+                logFile.append(new Date().toString() + ' - ' + f.absolutePath + '\n')
+            }
+
             fileContent = fileContent.replaceAll(pattern, newText)
             f.withWriter { writer -> writer.writeLine(fileContent) }
         }
